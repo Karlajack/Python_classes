eb85630186f9fbaeddc18076aa5d773e52e07e51
karlajack <jackmwenda@yahoo.com>
2024-02-20 21:50:06 +0300

    Revising input message

--- Week1 Project-Rock paper scissors/rockPS.py
+++ Week1 Project-Rock paper scissors/rockPS.py
@@ -2,8 +2,8 @@
 player1_score=0
 player2_score=0
 
-for play in range(5):
-    player1=input("Enter your outcome ").upper()
+for play in range(3):
+    player1=input("Enter your Play [Rock,Paper or Scissors] ").upper()
 
     from random import choice
     player2=choice(["ROCK","PAPER","SCISSORS"])
@@ -33,7 +33,7 @@ for play in range(5):
         player1_score=player1_score+1
         player2_score=player2_score+0
     else:
-        print("Finish")
+        print("Invalid input")
 if (player1_score>player2_score):
     print( f"player1_score is {player1_score},player2_score is {player2_score} = player1 is the  winner")
 elif (player1_score<player2_score):
